2c089572c93b2761475b1a3b2ca0d566ea3a4a62
youge325 <A372707325@126.com>
2026-03-19 20:57:24 +0800

    Revert "fix compiling error"
    This reverts commit 3043678228997f3651c085e3992c787eed4b050d.

--- setup.py
+++ setup.py
@@ -32,8 +32,6 @@ if __name__ == '__main__':
             )
             disable_nvshmem = True
     else:
-        # When NVSHMEM_DIR is provided (e.g. wheel install), resolve the actual SONAME.
-        nvshmem_host_lib = get_nvshmem_host_lib_name(nvshmem_dir)
         disable_nvshmem = False
 
     if not disable_nvshmem:
@@ -56,8 +54,7 @@ if __name__ == '__main__':
         include_dirs.extend([f'{nvshmem_dir}/include'])
         library_dirs.extend([f'{nvshmem_dir}/lib'])
         nvcc_dlink.extend(['-dlink', f'-L{nvshmem_dir}/lib', '-lnvshmem_device'])
-        # Keep device lib in dlink only; adding it here causes duplicate symbols.
-        extra_link_args.extend([f'-l:{nvshmem_host_lib}', f'-Wl,-rpath,{nvshmem_dir}/lib'])
+        extra_link_args.extend([f'-l:{nvshmem_host_lib}', '-l:libnvshmem_device.a', f'-Wl,-rpath,{nvshmem_dir}/lib'])
 
     if int(os.getenv('DISABLE_SM90_FEATURES', 0)):
         # Prefer A100
